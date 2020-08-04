f9e3908af9d76df133f4beba9a86244118a90a98
zoomrcd12fa <63201132+zoomrcd12fa@users.noreply.github.com>
2020-08-04 19:07:41 +0200

    Add Models of barrel and barrelset

--- vinegar_cellar/models.py
+++ vinegar_cellar/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
 
-# Create your models here.
+def current_year():
+    return datetime.date.today().year
+
+def max_value_current_year(value):
+    return MaxValueValidator(current_year())(value)
+
+class BarrelSet(models.Model):
+    year = models.IntegerField(('year'), validators=[MinValueValidator(1984), max_value_current_year])
+
+class Barrel(models.Model):
+    barrel_set = models.ForeignKey(BarrelSet, on_delete=models.CASCADE)
+    wood_type = models.CharField(max_length=50)
+    capability = models.PositiveIntegerField()
