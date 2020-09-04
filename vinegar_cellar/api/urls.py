418d92444f7f9d4121220902adaa15833bbe495a
zoomrcd12fa <63201132+zoomrcd12fa@users.noreply.github.com>
2020-09-04 15:09:50 +0200

    remove models useless

--- vinegar_cellar/api/urls.py
+++ vinegar_cellar/api/urls.py
@@ -1,15 +1,13 @@
 from rest_framework.routers import DefaultRouter
 
 
-from .views import (BarrelSetViewSet, BarrelViewSet, BarrelModelSet,
-                    OperationTypeViewSet, OperationViewSet, OperationModelSet)
+from .views import (BarrelSetViewSet, BarrelViewSet, OperationTypeViewSet,
+                    OperationViewSet)
 
 router = DefaultRouter()
 router.register(r'barrel_set', BarrelSetViewSet, basename='barrel_set')
 router.register(r'barrel', BarrelViewSet, basename='barrel')
-router.register(r'barmodel', BarrelModelSet, basename='barmodel')
 router.register(r'operation_type', OperationTypeViewSet,
                 basename='operation_type')
 router.register(r'operation', OperationViewSet, basename='operation')
-router.register(r'opmodel', OperationModelSet, basename='opmodel')
 urlpatterns = router.urls
